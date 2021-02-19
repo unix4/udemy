c6dcb7fde663b599f55930f35c5620c89f40f756
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-19 14:06:14 +0100

    Create sampletypes.ts
    sampletypes.ts

---
+++ Java/SpringBoot_Angular/01-typescript/02-variables/sampletypes.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Anup";
+let lastName: string = 'Kumar';
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " +  firstName + " " + lastName);
+
+// use template strings
+console.log(`${firstName} ${lastName}`);
