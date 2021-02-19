4dca09bdeb6598e8865ccb9b8f034c6ba303c5a7
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-19 14:13:59 +0100

    Create Driver.ts
    Driver.ts

---
+++ Java/SpringBoot_Angular/01-typescript/04-classes/05-modules/Driver.ts
@@ -0,0 +1,8 @@
+import {Customer05} from "./Customer";
+
+// Let's create an instance
+let myCustomer05 = new Customer05("Martin", "Dixon");
+
+
+console.log(myCustomer05.firstName);
+console.log(myCustomer05.lastName);
