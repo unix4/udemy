c86bd66a83d9cf98747ac3e9e2276902fac4ed96
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-19 14:03:02 +0100

    Delete Java/SpringBoot_Angular/01-helloworld directory
    Delete

--- Java/SpringBoot_Angular/01-helloworld/myhelloworld.ts
+++
@@ -1,8 +0,0 @@
-console.log("Hello World");
-console.log("Coding Gurus! " +  " Time for some fun!");
-
-let found: boolean = true;
-let grade: number = 88.6;
-let firstName: string = 'Marcel';
-
-console.log(firstName);
--- Java/SpringBoot_Angular/01-helloworld/sampletypes.ts
+++
@@ -1,9 +0,0 @@
-let found: boolean = true;
-let grade: number = 88.6;
-let firstName: string = "Marcel";
-let lastName: string = "LastName";
-
-console.log(found);
-console.log("The grade is " + grade);
-console.log("Hi " + firstName + " " + lastName);
-console.log(`Hi ${firstName} ${lastName}` );
