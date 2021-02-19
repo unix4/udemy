471262fbc6e3adb5fbf65c9b3368a26574b91524
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-19 14:00:44 +0100

    Create myhelloworld.ts
    myhelloworld.ts

---
+++ Java/SpringBoot_Angular/01-helloworld/myhelloworld.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log("Coding Gurus! " +  " Time for some fun!");
+
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = 'Marcel';
+
+console.log(firstName);
