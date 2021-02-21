f5a9f4433de107654dd71c47ec5e15a6a179d2cd
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-21 22:30:42 +0100

    Create JudoCoach.java
    JudoCoach

---
+++ SpringBoot/spring-demo-one/01/JudoCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+// Practice Activity
+public class JudoCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Do a lot of pushups";
+	}
+
+}
