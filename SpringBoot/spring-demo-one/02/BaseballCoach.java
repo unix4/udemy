86e076e125cb2a6f77d86e713769f9c35e5c1676
unix4 <69163299+unix4@users.noreply.github.com>
2021-02-22 11:40:47 +0100

    Create BaseballCoach.java
    BaseballCoach

---
+++ SpringBoot/spring-demo-one/02/BaseballCoach.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+	
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
+	
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+	
+	public String getDailyFortune() {
+		// use my fortuneService to get fortune
+		return fortuneService.getFortune();
+	}
+}
