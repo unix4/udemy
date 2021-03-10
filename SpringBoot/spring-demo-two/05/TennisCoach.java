dd01d1544976f0f4e482ba95c56c8bcb073d3491
unix4 <69163299+unix4@users.noreply.github.com>
2021-03-10 21:02:15 +0100

    Create TennisCoach.java

---
+++ SpringBoot/spring-demo-two/05/TennisCoach.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	// field injection
+	@Autowired
+	private FortuneService fortuneService;
+	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+}
